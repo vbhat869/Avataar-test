f534cf7b404bf920391a52ba78e4a9e2a3de36f6
vbhat869 <vinayaka.bhat@razorpay.com>
2023-09-04 22:13:58 +0530

    feat(avataar): minor fix

--- src/components/DynamicMenu.js
+++ src/components/DynamicMenu.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState, useRef } from "react";
 import SearchIcon from "../assets/search-svgrepo-com.svg";
 import arrow from "../assets/up-arrow-svgrepo-com.svg";
